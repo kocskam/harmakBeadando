6846e084739c787dc399665e32754955792c9bf4
kocskam <kocskam@gmail.com>
2020-04-25 19:32:44 +0200

    50-folyamatban

--- number50.py
+++ number50.py
@@ -1,10 +1,28 @@
-lawBreakers = []
-distance = int(input("Kérem az útszakasz hosszát: "))
-limit = int(input("Kérem a sebességkorlátot: "))
+#függvények
+def toSecundum(ms, min, hour):
+    return ms + min*60 + hour*3600
 
+def disIsOkay():
+    while True:
+        dis = input("Kérem az útszakasz hosszát: ")
+        if dis == "" or int(dis) < 1:
+            continue
+        return int(dis)
+
+def limIsOkay():
+    while True:
+        lim = input("Kérem a sebességkorlátot: ")
+        if limit == "" or int(lim) < 0:
+            continue
+        return int(lim)
+
+#program
+distance = disIsOkay()
+limit = limIsOkay()
+
+lawBreakers = []
 r = ""
 while r != "end":
-
     r = input("Beolvasás befejezéséhez írjon 'end'-et! ")
     if r == "end":
         break
@@ -12,15 +30,13 @@ while r != "end":
     #                                                                                                                      ellenőrizni, hogy a perc pl nem e több 60-nál
     # ido2 = ora2 + ":" + perc2 + ":" + ms2
 
-    sec1 = int(ms1) + int(perc1)*60 + int(ora1)*3600
-    sec2 = int(ms2) + int(perc2)*60 + int(ora2)*3600
+    sec1 = toSecundum(int(ms1), int(perc1), int(ora1))
+    sec2 = toSecundum(int(ms2), int(perc2), int(ora2))
 
     ido = int(sec2)-int(sec1)
 
     kisTav = distance/ido
     nagyTav = kisTav * 3.6
-
-    print(nagyTav)
 
     #   itt a kerekítést javítani
     # if isinstance(nagyTav, float):
@@ -34,4 +50,5 @@ while r != "end":
     if nagyTav > limit:
         lawBreakers.append(rendszam + " " + str(round(nagyTav, 2)) + "km/h")
 #                                                                                                                          üres tömböt lekezelni, kiírni szépen
+
 print(lawBreakers)
