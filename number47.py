25dfef064894edbacdda42193e729244816dc717
kocskam <kocskam@gmail.com>
2020-04-25 21:37:58 +0200

    Update number47.py

--- number47.py
+++ number47.py
@@ -1,7 +1,16 @@
 #Hogyan tároljam ezeket?
 
+while True:
+    try:
+        n = int(input("Hány sort kell beolvasnom? "))
+    except ValueError:
+        print("Hibás bemenet!")
+        continue
+    if n <= 0:
+        print("Hibás bemenet!")
+        continue
+    break
 
-n = int(input("Hány sort kell beolvasnom? "))
 print("Sorok beolvasása:")
 while n != 0:
     sor = input("A(z) " + str(n) +". sor: ")
