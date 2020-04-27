d7243522e62f76f58f5dc39695d8fe0d922dae6e
kocskam <kocskam@gmail.com>
2020-04-27 21:24:55 +0200

    test

--- number47.py
+++ number47.py
@@ -3,17 +3,40 @@
 while True:
     try:
         n = int(input("Hány sort kell beolvasnom? "))
-        if n <= 0:
+        if n < 0:
             raise ValueError
+        if n == 0:
+            break
     except ValueError:
         print("Hibás bemenet!")
         continue
     break
-
-print("Sorok beolvasása:")
+dict = {}
 while n != 0:
     sor = input("A(z) " + str(n) +". sor: ")
+
+    try:
+        number, hour, minute, text = sor.split(" ")
+        #textnél elszökik
+    except ValueError:
+        print("Hibás bemenet!")
+        continue
+
     n -= 1
-    number, hour, minute, text = sor.split(" ")
+
     time = hour + ":" + minute
     ready = time + " " + text
+    number = int(number)
+    ready = str(ready)
+
+    dict[number] = ready
+
+print(dict)
+find = 123
+
+for key, value in dict.items():
+    if key == find:
+        print("Na ez?", value)
+
+# x = sum(i == 123 for i in dict)
+# print(x)
